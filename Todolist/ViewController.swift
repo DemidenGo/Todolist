b9880048de50b68a6408a2c6d03fd0510c6b2a4f
Demidengo <dimidengo@yandex.ru>
2023-10-15 15:36:30 +0300

    Initial commit

---
+++ Todolist/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Todolist
+//
+//  Created by Юрий Демиденко on 15.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
